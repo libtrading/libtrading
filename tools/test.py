3117780285c2f527114fc88b1537b50dc0ea11f2
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2012-06-12 08:14:12 +0300

    tools: Rewrite test script
    Use the portable 'subprocess' module instead of the 'os' module
    for subprocess management. Also, use the standard indentation of
    4 spaces.
    
    Signed-off-by: Jussi Virtanen <jussi.k.virtanen@gmail.com>

--- tools/test.py
+++ tools/test.py
@@ -1,29 +1,15 @@
 #!/usr/bin/env python
 
-import os
-import signal
+import subprocess
 import sys
 import time
 
-def start(program, *args):
-  pid = os.fork()
-  if not pid:
-    os.execvp(program, (program,) +  args)
-  return pid
-
-def wait():
-  return os.wait()[1] >> 8
-
-def main():
-  server_pid = start("./trade", "server", "-p", "9000", "-c", "fix")
-  time.sleep(1)
-  result = 1
-  try:
-    start("./trade", "client", "localhost", "9000", "fix")
-    result = wait()
-  finally:
-    os.kill(server_pid, signal.SIGKILL)
+server = subprocess.Popen(["./trade", "server", "-p", "9000", "-c", "fix"])
+time.sleep(1)
+result = 1
+try:
+    client = subprocess.Popen(["./trade", "client", "localhost", "9000", "fix"])
+    result = client.wait()
+finally:
+    server.terminate()
     sys.exit(result)
-
-if __name__ == '__main__':
-  main()
