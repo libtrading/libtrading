e872c5d71848cba916a11659f75b955814e292da
Pekka Enberg <penberg@kernel.org>
2012-09-07 18:28:42 +0000

    Clean up buffer.c
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- buffer.c
+++ buffer.c
@@ -10,96 +10,91 @@
 
 struct buffer *buffer_new(unsigned long capacity)
 {
-	struct buffer *self;
+	struct buffer *buf;
 
-	self = malloc(sizeof *self + capacity);
-	if (!self)
+	buf = malloc(sizeof *buf + capacity);
+	if (!buf)
 		return NULL;
 
-	self->data	= (void *) self + sizeof(*self);
-	self->capacity	= capacity;
-	self->start	= 0;
-	self->end	= 0;
+	buf->data	= (void *) buf + sizeof(*buf);
+	buf->capacity	= capacity;
+	buf->start	= 0;
+	buf->end	= 0;
 
-	return self;
+	return buf;
 }
 
-void buffer_delete(struct buffer *self)
+void buffer_delete(struct buffer *buf)
 {
-	free(self);
+	free(buf);
 }
 
-uint8_t buffer_sum(struct buffer *self)
+uint8_t buffer_sum(struct buffer *buf)
 {
 	unsigned long sum = 0;
 	int i;
 
-	for (i = self->start; i < self->end; i++)
-		sum += self->data[i];
+	for (i = buf->start; i < buf->end; i++)
+		sum += buf->data[i];
 
 	return sum;
 }
 
-//! Formatted output to 'struct buffer'
-/*!
-  \return true if @format was appended to @self - false upon error of if
-          @format was truncated due to buffer being to small.
- */
-bool buffer_printf(struct buffer *self, const char *format, ...)
+bool buffer_printf(struct buffer *buf, const char *format, ...)
 {
 	size_t size;
 	va_list ap;
-	char *buf;
+	char *p;
 	int len;
 
-	buf	= buffer_end(self);
-	size	= buffer_remaining(self);
+	p	= buffer_end(buf);
+	size	= buffer_remaining(buf);
 
 	va_start(ap, format);
-	len = vsnprintf(buf, size, format, ap);
+	len = vsnprintf(p, size, format, ap);
 	va_end(ap);
 
 	if (len < 0 || len >= size)
 		return false;
 
-	self->end += len;
+	buf->end += len;
 
 	return true;
 }
 
-char *buffer_find(struct buffer *self, char delim)
+char *buffer_find(struct buffer *buf, char delim)
 {
-	while (buffer_first_char(self) != delim) {
-		if (!buffer_remaining(self))
+	while (buffer_first_char(buf) != delim) {
+		if (!buffer_remaining(buf))
 			return NULL;
 
-		buffer_advance(self, 1);
+		buffer_advance(buf, 1);
 	}
 
-	return buffer_start(self);
+	return buffer_start(buf);
 }
 
-ssize_t buffer_read(struct buffer *self, int fd)
+ssize_t buffer_read(struct buffer *buf, int fd)
 {
 	size_t count;
 	ssize_t len;
-	void *buf;
+	void *p;
 
-	buf		= buffer_end(self);
-	count		= buffer_remaining(self);
+	p	= buffer_end(buf);
+	count	= buffer_remaining(buf);
 
-	len = xread(fd, buf, count);
+	len = xread(fd, p, count);
 	if (len < 0)
 		return len;
 
-	self->end	+= len;
+	buf->end	+= len;
 
 	return len;
 }
 
-ssize_t buffer_write(struct buffer *self, int fd)
+ssize_t buffer_write(struct buffer *buf, int fd)
 {
-	return xwrite(fd, buffer_start(self), buffer_size(self));
+	return xwrite(fd, buffer_start(buf), buffer_size(buf));
 }
 
 void buffer_compact(struct buffer *buf)
