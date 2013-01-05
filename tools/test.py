609a863ecbd40a46e8492ea6048d1fb9a5347126
Marat Stanichenko <mstanichenko@gmail.com>
2013-01-05 14:25:08 +0400

    A sample FAST test case is included
    Run a sample FAST test case under the Makefile's 'check' target.
    
    Signed-off-by: Marat Stanichenko <mstanichenko@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- tools/test.py
+++ tools/test.py
@@ -8,7 +8,7 @@ import sys
 
 numfail = 0
 
-tests = [
+fix_tests = [
   #
   # FIX session level test cases. See Volume 2 of the FIX 4.4 specification for
   # details.
@@ -43,7 +43,7 @@ else:
   out = open('/dev/null', 'w')
   err = open('/dev/null', 'w')
 
-def popen_server():
+def popen_fix_server():
     args = [
       "./tools/fix/fix_server",
       "-p", "9000", 
@@ -51,7 +51,7 @@ def popen_server():
       "-f", "./tools/fix/scripts/%s" % test]
     return subprocess.Popen(args, stderr = err, stdout = out)
 
-def popen_client():
+def popen_fix_client():
     args = [
       "./tools/fix/fix_client",
       "-h", "localhost", 
@@ -60,15 +60,15 @@ def popen_client():
       "-f", "./tools/fix/scripts/%s" % test]
     return subprocess.Popen(args, stderr = err, stdout = out)
 
-for test in tests:
+for test in fix_tests:
     if verbose == 2:
         print "%s\n%s\n" % (test, "-" * len(test))
 
-    server = popen_server()
+    server = popen_fix_server()
 
     result = 1
     try:
-        client = popen_client()
+        client = popen_fix_client()
         result = client.wait()
     finally:
 	if result != 0:
@@ -95,12 +95,81 @@ for test in tests:
                 print status
 
 if numfail > 0:
-    print "\n" + red("Tests run: %d, Failures: %d" % (len(tests), numfail))
+    print "\n" + red("FIX tests run: %d, Failures: %d" % (len(fix_tests), numfail))
 else:
-    print "\nOK (%d tests)" % (len(tests))
+    print "\nOK (%d FIX tests)" % (len(fix_tests))
 
 if numfail != 0:
     result = 1
 else:
     result = 0
+
+if result != 0:
+    sys.exit(result)
+
+fast_tests = [
+  "MandatoryNoneOperator"
+]
+
+def popen_fast_server():
+    args = [
+      "./tools/fast/fast_server",
+      "-p", "9000",
+      "-c", "fast",
+      "-f", "./tools/fast/scripts/%s" % test]
+    return subprocess.Popen(args, stderr = err, stdout = out)
+
+def popen_fast_client():
+    args = [
+      "./tools/fast/fast_client",
+      "-h", "localhost",
+      "-p", "9000",
+      "-c", "fast",
+      "-f", "./tools/fast/scripts/%s" % test]
+    return subprocess.Popen(args, stderr = err, stdout = out)
+
+for test in fast_tests:
+    if verbose == 2:
+        print "%s\n%s\n" % (test, "-" * len(test))
+
+    server = popen_fast_server()
+
+    result = 1
+    try:
+        client = popen_fast_client()
+        result = client.wait()
+    finally:
+	if result != 0:
+            if verbose == 2:
+                status = "\n%s\n" % red("FAIL")
+            else:
+                status = "%-40s [%s]" % (test, red("FAIL"))
+
+            print status
+
+            numfail = numfail + 1
+
+            while server.poll() is None:
+                server.terminate()
+            while client.poll() is None:
+                client.terminate()
+        else:
+            if verbose == 2:
+                status = "\n%s\n" % green("PASS")
+            elif verbose == 1:
+                status = "%-40s [%s]" % (test, green("PASS"))
+
+            if verbose != 0:
+                print status
+
+if numfail > 0:
+    print "\n" + red("FAST tests run: %d, Failures: %d" % (len(fast_tests), numfail))
+else:
+    print "\nOK (%d FAST tests)" % (len(fast_tests))
+
+if numfail != 0:
+    result = 1
+else:
+    result = 0
+
 sys.exit(result)
