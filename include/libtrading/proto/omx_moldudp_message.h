34b179dcfe461279831056de454d1bc9c91bba35
Theo <theojepsen@gmail.com>
2017-09-28 11:19:01 -0700

    Added header for MoldUDP64

--- include/libtrading/proto/omx_moldudp_message.h
+++ include/libtrading/proto/omx_moldudp_message.h
@@ -33,6 +33,37 @@ static inline void *omx_moldudp_message_payload(struct omx_moldudp_message *msg)
 	return (void *) msg + sizeof(struct omx_moldudp_message);
 }
 
+/*
+ * MoldUDP64
+ */
+
+struct omx_moldudp64_header {
+	char			Session[10];
+	le64			SequenceNumber;
+	le16			MessageCount;
+} __attribute__((packed));
+
+struct omx_moldudp64_message {
+	le16			MessageLength;
+} __attribute__((packed));
+
+struct omx_moldudp64_request {
+	char			Session[10];
+	le64			SequenceNumber;
+	le16			RequestedMessageCount;
+} __attribute__((packed));
+
+static inline struct omx_moldudp64_message *omx_moldudp64_payload(struct omx_moldudp64_header *msg)
+{
+	return (void *) msg + sizeof(struct omx_moldudp64_header);
+}
+
+static inline void *omx_moldudp64_message_payload(struct omx_moldudp64_message *msg)
+{
+	return (void *) msg + sizeof(struct omx_moldudp64_message);
+}
+
+
 #ifdef __cplusplus
 }
 #endif
